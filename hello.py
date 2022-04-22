ae1cdd8863c562fcd82bdf46647e8c704f96cf83
mthanghoang <61542577+mthanghoang@users.noreply.github.com>
2022-04-22 22:48:08 +0300

    draw triangle

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
-print('hello')
-print('hello again')
-print('hello again')
+n = 6;
+for i in range(1, n+1):
+    for j in range(1, n+1-i):
+        print("", end = " ")
+    for k in range(1, i+1):
+        print("*", end=" ")
+    print()
